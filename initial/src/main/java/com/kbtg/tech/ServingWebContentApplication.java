4f376b551e099d9bb1e2e1582e084011c55a0c91
Kung Kub <kungcs20@gmail.com>
2020-12-15 22:46:26 +0700

    update lab

--- initial/src/main/java/com/kbtg/tech/ServingWebContentApplication.java
+++ initial/src/main/java/com/kbtg/tech/ServingWebContentApplication.java
@@ -1,31 +1,11 @@
-package com.kbtg.tech.controllers;
+package com.kbtg.tech;
 
-import com.kbtg.tech.services.OrderService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-     
-@Controller
-public class CheckOutController {
-
-    @Autowired
-    private OrderService orderService;
-
-    @GetMapping(value="/Checkout")
-    public String checkout() {
-        return "checkout";
-    }
-
-    @PostMapping(value="/Confirm")
-    public String confirm(@RequestParam Map<String, String> body, String name, Model model) {
-        String chargeNo = orderService.confirm(body.get("token"));
-        model.addAttribute("chargeNo", chargeNo);
-        return "confirm";
-    }
-
+@SpringBootApplication
+public class ServingWebContentApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentApplication.class, args);
+	}
 }
